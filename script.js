f09f6513b8c1cc21f7e16ab8af5b59c93d64c806
Seonkwan Yoo <seonkwanyoo@gmail.com>
2023-09-23 11:08:48 -0700

    Add height 100% to html and body to center contents

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const squareContainer = document.querySelector('.square-container')
 // Create element square and append it as a child of the container div
 const square = document.createElement('div');
-square.classList.add(square);
+square.classList.add('square');
 
 // Create a for-loop that will loop through and insert square into the square container
 let i = 16;
