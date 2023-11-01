b77c587795ea26278acfe1fc9fd717778ca1e9aa
Seonkwan Yoo <seonkwanyoo@gmail.com>
2023-11-01 06:05:18 -0700

    Create function to add individual grid

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+
 // Function will create a size of the grid of the user input
+function createGrid (rangeNum) {
+    for (let i = 0; i < rangeNum; i++){
+        let grid = document.createElement('div');
+        grid.className = 'grid'
+        document.querySelector('.grid-box').appendChild(grid)
+    }
+}
+
+createGrid(10)
