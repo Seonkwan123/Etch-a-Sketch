968d95c5226f36d01b130a8b5cafb91f43d6704b
Seonkwan Yoo <seonkwanyoo@gmail.com>
2023-11-02 05:34:06 -0700

    Add grid to grid box using loop

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 
 // Function will create a size of the grid of the user input
 function createGrid (rangeNum) {
-    for (let i = 0; i < rangeNum; i++){
+    for (let i = 0; i < (rangeNum ** 2); i++){
         let grid = document.createElement('div');
         grid.className = 'grid'
-        document.querySelector('.grid-box').appendChild(grid)
+        let gridBox = document.querySelector('.grid-box')
+        gridBox.appendChild(grid);
+        gridBox.style.gridTemplateColumns = `repeat(${rangeNum}, 1fr)`
+        gridBox.style.gridTemplateRows = `repeat(${rangeNum}, 1fr)`
     }
 }
-
 createGrid(10)
