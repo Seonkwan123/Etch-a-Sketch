aab3cd39143f5def560c29c01fc08fb1f2c8e298
Seonkwan Yoo <seonkwanyoo@gmail.com>
2023-11-02 06:14:50 -0700

    Create eventListener to change grid size
    eventListener was applied to the slider to change the grid size.

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-
+let slider = document.querySelector("input[type='range'")
+slider.addEventListener('input', createGrid)
 // Function will create a size of the grid of the user input
-function createGrid (rangeNum) {
-    for (let i = 0; i < (rangeNum ** 2); i++){
+function createGrid () {
+    let sliderValue = slider.value
+    console.log(sliderValue)
+    for (let i = 0; i < (sliderValue ** 2); i++){
         let grid = document.createElement('div');
         grid.className = 'grid'
         let gridBox = document.querySelector('.grid-box')
         gridBox.appendChild(grid);
-        gridBox.style.gridTemplateColumns = `repeat(${rangeNum}, 1fr)`
-        gridBox.style.gridTemplateRows = `repeat(${rangeNum}, 1fr)`
+        gridBox.style.gridTemplateColumns = `repeat(${sliderValue}, 1fr)`
+        gridBox.style.gridTemplateRows = `repeat(${sliderValue}, 1fr)`
     }
 }
-createGrid(10)
