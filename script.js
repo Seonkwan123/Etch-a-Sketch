7581b1dc48ddfc3f649f4f48e144a65bfb74c30c
Seonkwan Yoo <seonkwanyoo@gmail.com>
2023-11-03 05:37:01 -0700

    Initial value added to slider label

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 let slider = document.querySelector("input[type='range'")
+let sliderValue = slider.value
 slider.addEventListener('input', createGrid)
+let sliderLabel = document.querySelector('label')
+sliderLabel.innerText = `${sliderValue} x ${sliderValue}`
 // Function will create a size of the grid of the user input
 function createGrid () {
-    let sliderValue = slider.value
-    console.log(sliderValue)
-    for (let i = 0; i < (sliderValue ** 2); i++){
+        sliderValue = slider.value
+    for(let i = 0; i < (sliderValue ** 2); i++){
         let grid = document.createElement('div');
         grid.className = 'grid'
         let gridBox = document.querySelector('.grid-box')
         gridBox.appendChild(grid);
+        sliderLabel.innerText = `${sliderValue} x ${sliderValue}`
         gridBox.style.gridTemplateColumns = `repeat(${sliderValue}, 1fr)`
         gridBox.style.gridTemplateRows = `repeat(${sliderValue}, 1fr)`
     }
