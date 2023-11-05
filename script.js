86fc0b57003cc5fff472d4bc0ca7ec2ff0346829
Seonkwan Yoo <seonkwanyoo@gmail.com>
2023-11-05 05:47:40 -0800

    Add function draw and delete forEach loop
    Draw function changes background color if mousedown and
    mouseover both is satisfied. if not, function will not
    execute. ForEach loop was deleted and just moved addevenlistener
    inside the the for-loop inself to always assign toe eventListener even
    if the grid size changes.

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const sliderLabel = document.querySelector('label')
 const gridBox = document.querySelector('.grid-box')
 const colorSelector = document.querySelector('#color-selector');
 let gridElements;
-let grid;
 
 // Add inline style to change slider label text and gridbox size 
 let sliderValue = slider.value
@@ -16,34 +15,29 @@ createGrid();
 
 
 let mouseDown = false;
-document.body.addEventListener('mousedown',() => {
-    mouseDown = true;
-})
-document.body.addEventListener('mouseup',() => {
-    mouseDown = false;
-})
+document.body.onmousedown = () => {mouseDown = true;}
+document.body.onmouseup = () => mouseDown = false;
 
 // This eventListener changes grid slize and slider label when slider is used.
 slider.addEventListener('input', createGrid)
 // Function will create a size of the grid of the user input
 function createGrid () {
 sliderValue = slider.value
+sliderLabel.innerText = `${sliderValue} x ${sliderValue}`
+gridBox.style.gridTemplateColumns = `repeat(${sliderValue}, 1fr)`
+gridBox.style.gridTemplateRows = `repeat(${sliderValue}, 1fr)`
 for(let i = 0; i < (sliderValue ** 2); i++){
-    grid = document.createElement('div');
+    let grid = document.createElement('div');
     grid.className = 'grid-element'
     gridBox.appendChild(grid);
     gridElements = document.querySelectorAll('.grid-element');
-    sliderLabel.innerText = `${sliderValue} x ${sliderValue}`
-    gridBox.style.gridTemplateColumns = `repeat(${sliderValue}, 1fr)`
-    gridBox.style.gridTemplateRows = `repeat(${sliderValue}, 1fr)`
+    grid.addEventListener('mouseover', draw)
     }
 }
 
-gridElements.forEach(element => {
-    element.addEventListener('mouseover',draw)
-})
-
 function draw (e) {
     if (mouseDown === true && e.type == 'mouseover') {
+        e.target.style.backgroundColor = colorSelector.value;
+        console.log(gridElements)
     }
 }
