d34b9433bdcaf8a16883dc6cd51de3aeef7b9a45
Seonkwan Yoo <seonkwanyoo@gmail.com>
2023-11-05 05:18:18 -0800

    Add mouseDown variable to check mouse status

--- script.js
+++ script.js
@@ -1,33 +1,49 @@
-// Set initial grid box size and slider label texts as default value
-let slider = document.querySelector("input[type='range'")
-let sliderValue = slider.value
-let sliderLabel = document.querySelector('label')
-let gridBox = document.querySelector('.grid-box')
-// initially invoke function to generate div inside grid
-createGrid();
+const slider = document.querySelector("input[type='range'")
+const sliderLabel = document.querySelector('label')
+const gridBox = document.querySelector('.grid-box')
+const colorSelector = document.querySelector('#color-selector');
+let gridElements;
+let grid;
+
 // Add inline style to change slider label text and gridbox size 
+let sliderValue = slider.value
 sliderLabel.innerText = `${sliderValue} x ${sliderValue}`
 gridBox.style.gridTemplateColumns = `repeat(${sliderValue}, 1fr)`
 gridBox.style.gridTemplateRows = `repeat(${sliderValue}, 1fr)`
+
+// initially invoke function to generate div inside grid
+createGrid();
+
+
+let mouseDown = false;
+document.body.addEventListener('mousedown',() => {
+    mouseDown = true;
+})
+document.body.addEventListener('mouseup',() => {
+    mouseDown = false;
+})
+
 // This eventListener changes grid slize and slider label when slider is used.
 slider.addEventListener('input', createGrid)
 // Function will create a size of the grid of the user input
 function createGrid () {
 sliderValue = slider.value
 for(let i = 0; i < (sliderValue ** 2); i++){
-    let grid = document.createElement('div');
-    grid.className = 'grid'
+    grid = document.createElement('div');
+    grid.className = 'grid-element'
     gridBox.appendChild(grid);
+    gridElements = document.querySelectorAll('.grid-element');
     sliderLabel.innerText = `${sliderValue} x ${sliderValue}`
     gridBox.style.gridTemplateColumns = `repeat(${sliderValue}, 1fr)`
     gridBox.style.gridTemplateRows = `repeat(${sliderValue}, 1fr)`
     }
 }
 
+gridElements.forEach(element => {
+    element.addEventListener('mouseover',draw)
+})
 
-// Select color buttons
-let colorSelector = document.querySelector('#color-selector');
-grid.addEventListener('click', sketchColor)
-function sketchColor () {
-    grid.style.color = colorSelector.value;
+function draw (e) {
+    if (mouseDown === true && e.type == 'mouseover') {
+    }
 }
